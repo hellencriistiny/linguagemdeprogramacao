a530a4f6b3705d5c51de850a309cd7db04cba069
hellencriistiny <115796382+hellencriistiny@users.noreply.github.com>
2023-06-01 15:25:48 -0300

    Create variasfuncoes.c

---
+++ variasfuncoes.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop 
+
+int main(int argc, char** argv) {
+	return 0;
+}*/
+
+/*void F1(int *vet)
+{
+	int i;
+	for(i=0;i<10;i++)
+	{
+		*vet =i;
+		vet++;
+	}
+	printf("Valor de vet: %d", vet);
+}
+int main()
+{
+	int vet[10];
+	F1(vet);
+	
+}*/
+
+
+/*//Passagem de Parâmetro por VALOR
+
+void F1(int x)
+{
+	x = 100;
+}
+int main()
+{
+	printf("Passagem de Parametro por Valor\n");
+	int a;
+	a =10;
+	F1(a);
+	printf("O valor de a: %d",a);
+	
+}*/
+
+/*//Passagem de Parâmetro por Referência
+void F1(int *x)
+{
+	*x = 1000;
+
+}
+int main()
+{
+	int a;
+	a =10;
+	F1(&a);
+	printf("Valor de a: %d",a);
+}*/
