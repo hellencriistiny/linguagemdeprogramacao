cad936ee87d58d47220759c22a6c6b86a0b847c4
hellencriistiny <115796382+hellencriistiny@users.noreply.github.com>
2023-06-12 16:00:29 -0300

    Create AULADEC.c

---
+++ AULADEC.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main() {
+	/*
+	printf("Oi, tudo bem? Tenho 6 anos e programo. \n");
+	
+	printf("Valor inteiro: %d.\n", 10);
+	printf("Valor real: %f.\n", 3.14159265);
+	printf("Valor real com apenas duas casas: %.2f. \n", 3.14159265);
+	printf("Dado de texto: %c.\n",'a');
+	printf("Dado de texto: %s.\n", "testando");
+	*/
+	
+	
+	/*
+	int idade = 0;
+	
+	printf("Valor inicial da idade: %d.\n", idade);
+	
+	printf("Digite uma idade:\n");
+	scanf("%d", &idade);
+	
+	printf("Idade informada:%d.\n", idade);
+	*/
+	
+	//NÃO É CERTO DE SER FEITO
+	/*int idade = 0;
+	int ano = 1950;
+	
+	printf("Valor inicial da idade: %d.\n", idade);
+	printf("Valor inicial do ano: %d.\n", ano);
+	
+	printf("Digite uma idade:\n");
+	scanf("%d %d", &idade, &ano);
+	
+	printf("Idade informada: %d.\n", idade);
+	printf("Ano informado: %d.\n", ano);
+	*/
+	
+	//O CORRETO DE SER FEITO
+	/*int idade = 0;
+	float peso = 0.0;
+	
+	printf("Digite uma idade:\n");
+	scanf("%d", &idade);
+	
+	printf("Digite o peso desejado:\n");
+	scanf("%f", &peso);
+	
+	printf("Idade informada: %d.\n", idade);
+	printf("Peso informado: %.2f.\n", peso);
+	*/
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
