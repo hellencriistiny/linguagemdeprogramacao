e2c5ddfe524be1c8c3b89d1bd196ae47966e44aa
hellencriistiny <115796382+hellencriistiny@users.noreply.github.com>
2023-06-22 17:25:32 -0300

    Create AULADEC2.c

---
+++ AULADEC2.c
@@ -0,0 +1,76 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+	/*int d;
+	
+	printf("Insira um valor de 1 a 7: \n");
+	scanf("%d", &d);
+	
+	switch(d){
+		case 1:
+			printf("Domingo.\n");
+			break;
+		case 2:
+			printf("Segunda-feira.\n");
+			break;
+		case 3:
+			printf("Terca-feira.\n");
+			break;
+		case 4:
+			printf("Quarta-feira.\n");
+			break;
+		case 5:
+			printf("Quinta-feira.\n");
+			break;
+		case 6:
+			printf("Sexta-feira.\n");
+			break;
+		case 7:
+			printf("Sabado.\n");
+			break;
+		default:
+			printf("Valor inválido.\n");
+			break;
+	}*/
+	
+	/*float num1, num2, subt, resultado;
+	int opcao;
+	
+	printf("Insira o primeiro numero:\n");
+	scanf("%f", &num1);
+	
+	printf("Insira o segundo numero:\n");
+	scanf("%f", &num2);
+	
+	printf("MENU\n 1 - Diferenca entre os dois numeros.\n 2 - Média entre os numeros digitados.\n 3 - Divisao do primeiro pelo segundo.\n 4 - Divisao do segundo pelo primeiro.\n");
+	
+	printf("Escolha a opcao desejada:\n");
+	scanf("%d", &opcao);
+	
+	switch(opcao){
+		case 1:
+			resultado = num1-num2;
+			printf("Diferenca entre num1 e num2:%.2f", resultado);
+			break;
+		case 2:
+			resultado = (num1+num2)/2;
+			printf("Media dos numeros: %.2f", resultado);
+			break;
+		case 3:
+			resultado = num1/num2;
+			printf("Divisao do num1 por num2: %.2f", resultado);
+			break;
+		case 4:
+			resultado = num2/num1;
+			printf("Divisao de num2 por num1: %.2f", resultado);
+			break;
+		default:
+			("Esse valor nao foi encontrado.\n");
+	}
+	scanf("%f", &resultado);*/
+	
+	
+
+
+}
